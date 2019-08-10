89625e5bd6823d78e1f72f67c00baea4ea06f8f1
Edwin Mak <edwinthinks@gmail.com>
2019-08-10 15:55:05 -0400

    Fix the stuff! (#5)

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 import './js/components/container/FormContainer.jsx'
-import './js/components/container/TodoList.jsx'
+import TodoList from './js/components/container/TodoList.jsx'
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const wrapper = document.getElementById('todo-list')
+ReactDOM.render(<TodoList />, wrapper)
